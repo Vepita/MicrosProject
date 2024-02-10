f9ee4ff9d884335774e7b3822f9747fd01834ff2
Vepita <35640194+Vepita@users.noreply.github.com>
2024-02-10 12:11:17 -0500

    T1: config first hello world

---
+++ src/main/java/com/ec/litethinking/LitethinkingProjectApplication.java
@@ -0,0 +1,14 @@
+package com.ec.litethinking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitethinkingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitethinkingProjectApplication.class, args);
+		System.out.println("HOLA MI NOMBRE ES VERO");
+	}
+
+}
